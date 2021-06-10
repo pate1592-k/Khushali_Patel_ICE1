51be87a677fc28abbb530ef120420a719c1d5f22
pate1592-k <78705437+pate1592-k@users.noreply.github.com>
2021-06-09 23:39:02 -0400

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,4 +1,5 @@
-/*
+/* Name: Khushaliben Patel
+ * Student number #991543104
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
